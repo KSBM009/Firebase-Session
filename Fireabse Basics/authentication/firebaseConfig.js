90d8fc10a0531b109c01cc1e9ec809740d12f5a9
Advaith Manoj <89682610+Advaith-dev@users.noreply.github.com>
2024-04-09 10:49:44 +0530

    Update firebaseConfig.js

--- Fireabse Basics/authentication/firebaseConfig.js
+++ Fireabse Basics/authentication/firebaseConfig.js
@@ -6,14 +6,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/fireba
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDL2M4uosgr6p9u2esXumESEyMolmSCdZ0",
-    authDomain: "basics-of-firebase-4bd2b.firebaseapp.com",
-    databaseURL: "https://basics-of-firebase-4bd2b-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "basics-of-firebase-4bd2b",
-    storageBucket: "basics-of-firebase-4bd2b.appspot.com",
-    messagingSenderId: "388250151466",
-    appId: "1:388250151466:web:4f3f564265864322923153",
-    measurementId: "G-7WFN5J6FF5"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
